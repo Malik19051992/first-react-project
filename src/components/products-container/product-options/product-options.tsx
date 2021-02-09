ba90ea995b31bb52f087da1467053c4bfca6a5b2
Malik <valentin.hruzinski@smartexlab.com>
2021-02-09 14:17:37 +0300

    [fist-project]: added sorting

--- src/components/products-container/product-options/product-options.tsx
+++ src/components/products-container/product-options/product-options.tsx
@@ -1,4 +1,3 @@
-import * as _ from 'lodash';
 import React from 'react';
 
 import './product-options.scss';
@@ -6,28 +5,37 @@ import gridIcon from '../../../assets/images/grid.svg';
 import listIcon from '../../../assets/images/list.svg';
 import { ViewMode, ViewModeUtils } from '../../../utils/view-mode.utils';
 
-export class ProductOptions extends React.Component<{ viewModeChanged: any }, { viewMode: ViewMode }> {
+export enum SortOption {
+  TITLE = 'title',
+  RATING = 'rating',
+  FROM_CHEEP_TO_EXPENSIVE = 'fromCheepToExpensive',
+  FROM_EXPENSIVE_TO_CHEEP = 'fromExpensiveToCheep'
+}
+
+export class ProductOptions extends React.Component<{ viewModeChanged: (value: ViewMode) => void, sortOptionChanged: (optin: SortOption) => void },
+  { viewMode: ViewMode, sortOption: SortOption }> {
   constructor(props: any) {
     super(props);
   }
 
   componentDidMount() {
-    this.setState({ viewMode: ViewModeUtils.getViewMode() })
+    this.setState({ viewMode: ViewModeUtils.getViewMode(), sortOption: SortOption.TITLE })
   }
 
   render() {
     return (<div className='product-options-container'>
       <div className="sorting-container">
-        <select>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
+        <select onChange={(event) => {this.sortOptionChangeHandler(event.target.value as SortOption)}}>
+          <option selected value={SortOption.TITLE}>По названию</option>
+          <option value={SortOption.RATING}>По рейтингу</option>
+          <option value={SortOption.FROM_CHEEP_TO_EXPENSIVE}>Сначала дешовые</option>
+          <option value={SortOption.FROM_EXPENSIVE_TO_CHEEP}>Сначала дорогие</option>
         </select>
       </div>
       <div className="view-switcher">
-        <img src={gridIcon} className={this.state ?.viewMode === ViewMode.GRID ? 'active' : ''}
+        <img src={gridIcon} className={this.state?.viewMode === ViewMode.GRID ? 'active' : ''}
              onClick={() => this.viewModeChanged(ViewMode.GRID)}/>
-        <img src={listIcon} className={this.state ?.viewMode === ViewMode.LIST ? 'active' : ''}
+        <img src={listIcon} className={this.state?.viewMode === ViewMode.LIST ? 'active' : ''}
              onClick={() => this.viewModeChanged(ViewMode.LIST)}/>
       </div>
     </div>);
@@ -37,5 +45,9 @@ export class ProductOptions extends React.Component<{ viewModeChanged: any }, { 
     ViewModeUtils.setViewMode(value);
     this.setState({ viewMode: value });
     this.props.viewModeChanged(value);
+  }
+
+  private sortOptionChangeHandler(value: SortOption) {
+    this.props.sortOptionChanged(value);
   }
 }
--- src/components/products-container/products-container.tsx
+++ src/components/products-container/products-container.tsx
@@ -8,10 +8,10 @@ import { Category } from '../../entries/category';
 import { getProducts, getCategories } from '../../redux/actions';
 import { ProductGridItem } from '../product-grid-item/product-grid-item';
 import { ProductListItem } from '../product-list-item/product-list-item';
-import { ProductOptions } from './product-options/product-options';
+import { ProductOptions, SortOption } from './product-options/product-options';
 import { ViewMode, ViewModeUtils } from '../../utils/view-mode.utils';
 
-export class ProductsContainer extends React.Component<{ match: any, location: any }, { products: Product[], viewMode: ViewMode }> {
+export class ProductsContainer extends React.Component<{ match: any, location: any }, { products: Product[], viewMode: ViewMode, sortOption: SortOption }> {
   constructor(props: any) {
     super(props);
   }
@@ -27,7 +27,7 @@ export class ProductsContainer extends React.Component<{ match: any, location: a
 
   componentWillReceiveProps(newProps) {
     // need to update after url update. kostyl
-    setTimeout(()=>{
+    setTimeout(() => {
       this.setCategoryProducts();
     });
   }
@@ -35,7 +35,8 @@ export class ProductsContainer extends React.Component<{ match: any, location: a
   render() {
     return (
       <div className='product-container-wrapper'>
-        <ProductOptions viewModeChanged={this.viewModeChanged.bind(this)}></ProductOptions>
+        <ProductOptions viewModeChanged={this.viewModeChanged.bind(this)}
+                        sortOptionChanged={this.sortOptionChanged.bind(this)}></ProductOptions>
         <div className={this.state ?.viewMode === ViewMode.GRID ? 'product-container-grid' : 'product-container-list'}>
           {_.map(this.state ?.products, (product: Product) => {
             if(this.state?.viewMode === ViewMode.GRID){
@@ -52,7 +53,7 @@ export class ProductsContainer extends React.Component<{ match: any, location: a
     const categoryId: number = +this.props.match.params.categoryId;
 
     if (!categoryId) {
-      this.setState({ products: this.filterProducts(getProducts()) });
+      this.setState({ products: this.sortProducts(this.filterProducts(getProducts()), this.state?.sortOption || SortOption.TITLE) });
 
       return;
     }
@@ -60,7 +61,7 @@ export class ProductsContainer extends React.Component<{ match: any, location: a
     const categories: Category[] = getCategories();
     const categoryProducts: Product[] = this.getCategoryProducts(categoryId, categories, getProducts());
 
-    this.setState({ products: this.filterProducts(categoryProducts) });
+    this.setState({ products: this.sortProducts(this.filterProducts(categoryProducts), this.state?.sortOption || SortOption.TITLE) });
   }
 
   private getCategoryProducts(categoryId: number, categories: Category[], products: Product[]): Product[] {
@@ -76,7 +77,10 @@ export class ProductsContainer extends React.Component<{ match: any, location: a
 
   private viewModeChanged(value: ViewMode) {
     this.setState({ viewMode: value })
+  }
 
+  private sortOptionChanged(sortOption: SortOption) {
+    this.setState({ products: this.sortProducts(this.state.products, sortOption), sortOption });
   }
 
   private filterProducts(products: Product[]) {
@@ -87,5 +91,18 @@ export class ProductsContainer extends React.Component<{ match: any, location: a
     }
 
     return _.filter(products, (product: Product) => product.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase()));
+  }
+
+  private sortProducts(producsts: Product[], sortOption: SortOption) {
+    switch (sortOption) {
+      case SortOption.TITLE:
+        return _.sortBy(producsts, 'title');
+      case SortOption.RATING:
+        return _.sortBy(producsts, 'rating').reverse();
+      case SortOption.FROM_CHEEP_TO_EXPENSIVE:
+        return _.sortBy(producsts, 'price');
+      case SortOption.FROM_EXPENSIVE_TO_CHEEP:
+        return _.sortBy(producsts, 'price').reverse();
+    }
   }
 }
